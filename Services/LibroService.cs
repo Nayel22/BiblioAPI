0ab7cfa3e620594b3941907eb5ce55b6fc791d5e
Nayel22 <nayelsolorzanom213@gmail.com>
2025-04-18 14:56:27 -0600

    cambniios

---
+++ Services/LibroService.cs
@@ -0,0 +1,14 @@
+﻿namespace BiblioAPI.Services
+{
+    public class LibroService
+    {
+
+        private readonly string _connectionString;
+
+        public LibroService(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("MiConexion");
+        }
+
+    }
+}
